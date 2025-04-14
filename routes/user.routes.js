a900cb1d4dc8e564f8f3194745379a2b684d5435
praveen378 <115167547+praveen378@users.noreply.github.com>
2025-04-14 11:11:31 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,13 +9,14 @@ import {
 } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middleware/auth.middleware.js";
 import { sendMessage } from "../controllers/message.controller.js";
-
+import { generateTwilioToken } from "../controllers/video.controller.js";
 router.post("/register", register);
 router.post("/login", login);
 router.get("/get-profile", isAuthenticated, getProfile);
 router.post("/logout", isAuthenticated, logout);
 router.post("/messge", isAuthenticated, sendMessage);
 router.get("/get-other-users", isAuthenticated, getOtherUsers);
- 
+ // ✅ New route for Twilio Video token
+router.post("/video/token", isAuthenticated, generateTwilioToken);
 
 export default router;
